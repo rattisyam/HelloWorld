6dbe8a0d7011f4e2d3d87d629743cf3c1cc9f01d
rattisyam <syam098@gmail.com>
2021-04-29 14:56:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,64 +1,8 @@
-variable "location" {
-  type = string
-  default = "south india"
-}
-variable "vm_name" {
-  type = string
-  default = "vm02"
-}
-variable "resource_group_name" {
-  type = string
-  default = "Learning"
-}
-variable "vnetRGroup" {
+variable "location"{
   type = string
 }
-variable "admin_username" {
+variable "rgName"{
   type = string
-  default = "winadmin"
-}
-variable "vm_sku" {
-  type = string
-  default = "2016-Datacenter"
-}
-variable "admin_password" {
-  type = string
-}
-variable "vm_size" {
-  type = string
-  default = "Standard_DS1_v2"
-}
-variable "os_data_disk_size_in_gb" {
-  type = number
-  default = 127
-}
-variable "data_disks" {
-  type = list
-  default = [60]
-}
-variable "boot_diag_storage_account" {
-  type = string
-  default = null
-}
-variable "vnet_name" {
-  type = string
-  default = "vnet02"
-}
-variable "subnet_name" {
-  type = string
-  default = "subnet02"
-}
-variable "availability_set" {
-  type = string
-  default = null
-}
-variable "identity_id" {
-  type = list
-  default = []
-}
-variable "identity" {
-  type = string
-  default = "no"
 }
 variable "client_id" {
   type = string
@@ -72,92 +16,43 @@ variable "subscription_id" {
 variable "tenant_id" {
   type = string
 }
-locals {
-
-}
-
-provider "azurerm" {
-
-	 client_id = var.client_id
-	 client_secret = var.client_secret
-	 subscription_id = var.subscription_id
-	 tenant_id = var.tenant_id
-	 features {}
-}
 terraform {
   backend "azurerm" {
     resource_group_name  = "32943"
     storage_account_name = "cloudlegonewstorage"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
-    access_key = "y3yRwsDZoqoz20V6ZlRN3ImBzWXu6BuyUovv13cyfJQKMhtJC2QPa/FxNKTtVDMTwBbbsq9cws2c6U8V7//2oQ=="
-    sas_token  = "sp=r&st=2021-04-28T11:52:24Z&se=2021-04-28T19:52:24Z&spr=https&sv=2020-02-10&sr=c&sig=47BDPYHa23pM76hMM5pv0Znam7WHDJasF2RYpJOslpU%3D"
   }
 }
-data "azurerm_subnet" "vmSubnet" {
-  name = var.subnet_name
-  resource_group_name = var.vnetRGroup
-  virtual_network_name = var.vnet_name
+provider "azurerm" {
+	 version = "~>2.0"
+	 features {}
 }
-data "azurerm_storage_account" "bootDiag" {
-  name = var.boot_diag_storage_account
-  resource_group_name = var.resource_group_name
-}
-resource "azurerm_network_interface" "nic01" {
-  location = var.location
-  name = join("_",[var.vm_name,"nic"])
-  resource_group_name = var.resource_group_name
-  ip_configuration {
-    name = join("_",[var.vm_name,"ip"])
-    subnet_id = data.azurerm_subnet.vmSubnet.id
-    private_ip_address_allocation = "dynamic"
-  }
-}
-resource "azurerm_virtual_machine" "mvm01" {
-  name = var.vm_name
-  location = var.location
-  resource_group_name = var.resource_group_name
-  network_interface_ids = [azurerm_network_interface.nic01.id]
-  vm_size = var.vm_size
-  availability_set_id = var.availability_set
-  os_profile_windows_config {
+resource "azurerm_virtual_network" "example" {
+  name                = "virtualNetwork1"
+  location            = var.location
+  resource_group_name = var.rgName
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
+  
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
   }
-  storage_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer = "WindowsServer"
-    sku = var.vm_sku
-    version = "latest"
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
   }
-  storage_os_disk {
-    create_option = "FromImage"
-    name = join("_",[var.vm_name,"osDisk"])
-    disk_size_gb = var.os_data_disk_size_in_gb
+
+  subnet {
+    name           = "subnet3"
+    address_prefix = "10.0.3.0/24"
+    
   }
-  dynamic "storage_data_disk" {
-    for_each = [for d in var.data_disks :{
-      diskname = join("_",[var.vm_name,"datadisk",index(var.data_disks,d )])
-      disklun = index(var.data_disks,d )
-      disksizegb = d
-      createoption = "Empty"
-    }]
-    content {
-      create_option = storage_data_disk.value.createoption
-      lun = storage_data_disk.value.disklun
-      name = storage_data_disk.value.diskname
-      disk_size_gb = storage_data_disk.value.disksizegb
-    }
-  }
-  os_profile {
-    admin_username = var.admin_username
-    admin_password = var.admin_password
-    computer_name = var.vm_name
-  }
-  identity {
-    type = length(regexall("yes",var.identity )) > 0 ? "UserAssigned":null
-  }
-  boot_diagnostics {
-    enabled = var.boot_diag_storage_account == null ? false:true
-    storage_uri = var.boot_diag_storage_account == null ? null:data.azurerm_storage_account.bootDiag.primary_blob_endpoint
+
+  tags = {
+    environment = "Production"
   }
 }
